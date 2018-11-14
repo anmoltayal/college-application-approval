ac15f4a8841fbe6ee7d83744c88d7a0c2a3e51c1
Anmol <anmoltayal10@gmail.com>
2018-11-14 23:08:02 +0530

    college application approval

---
+++ project1/src/java/servlets/Login.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Anmol Tayal
+ */
+public class Login extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+    {
+        String id=request.getParameter("username");
+        String password = request.getParameter("pass");
+        PrintWriter out=response.getWriter();
+        out.println(id+" "+password);
+        try {
+            LoginDAO l = new LoginDAO();
+            String result = l.checkInfo(id, password);
+            out.println(result);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
